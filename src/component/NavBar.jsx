c5d2c59cb07f9d1ddfbe7e2ee0414eaadf3cdcdf
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2023-07-07 01:51:06 +0200

    navbar

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,40 @@
+import "./navbar.css";
+import logo from "../assets/Netflix_2015_logo.svg.png";
+import { useEffect, useState } from "react";
+
+const NavBar = () => {
+  const [show, setScroll] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 30) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <nav className={`navbar ${show && "navbar__black"}`}>
+      <ul className="navbar__ul">
+        <a href="/">
+          <img src={logo} alt="company logo" className="navbar__logo" />
+        </a>
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="avatar"
+            className="navbar__avatar"
+          />
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../component/NavBar";
+import "./homescreen.css";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <NavBar />
+    </div>
+  );
+};
+
+export default HomeScreen;
